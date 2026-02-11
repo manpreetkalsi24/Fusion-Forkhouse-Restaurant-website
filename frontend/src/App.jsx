a921350a17a49ce455d402b9cec68dd7b15b52a2
Manpreet-Kaur-8975967 <mkaur5967@conestogac.on.ca>
2026-02-11 14:36:23 -0500

    Worked on header and footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Header";
+import Footer from "./components/Footer";
+import "./main.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/write-review" element={<WriteReview />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
