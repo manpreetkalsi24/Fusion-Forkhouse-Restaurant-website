bdfbfd19a57070746dc3614e2ec7627dd9d7f793
Manpreet-Kaur-8975967 <mkaur5967@conestogac.on.ca>
2026-02-19 12:09:10 -0500

    Worked on Menu page

---
+++ frontend/src/pages/Menu.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Menu() {
+  return (
+    <section className="menu-page">
+
+      {/* Page Title */}
+      <div className="menu-header">
+        <h1>Our Menu</h1>
+        <p>
+          Explore a curated selection of delicious dishes crafted with the
+          freshest ingredients to satisfy every palate.
+        </p>
+      </div>
