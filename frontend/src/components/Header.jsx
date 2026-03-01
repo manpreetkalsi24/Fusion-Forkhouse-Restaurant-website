0ae5d456616181ec345cafcd48824cf5c52776f2
Manpreet-Kaur-8975967 <mkaur5967@conestogac.on.ca>
2026-03-01 07:57:50 -0500

    Hamburger menu added for mobile screen and resposnive code for other screens

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="navbar">
       <div className="navbar-container">
         {/* Logo */}
-        <Link to="/" className="logo">Fusion Forkhouse</Link>
+        <Link to="/" className="logo" onClick={closeMenu}>
+          Fusion Forkhouse
+        </Link>
+
+        <div className={`hamburger ${menuOpen ? "active" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
 
         {/* Nav Links */}
-        <nav>
+        <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
           <ul className="nav-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/menu">Menu</Link></li>
-            <li><Link to="/reservations">Reservations</Link></li>
-            {/* <li><Link to="/write-review">Write Review</Link></li> */}
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+            <li><Link to="/about" onClick={closeMenu}>About Us</Link></li>
+            <li><Link to="/menu" onClick={closeMenu}>Menu</Link></li>
+            <li><Link to="/reservations" onClick={closeMenu}>Reservations</Link></li>
+            <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
           </ul>
+
+          <Link to="/reservations" className="book-btn mobile-btn" onClick={closeMenu}>
+            Book Table
+          </Link>
         </nav>
 
         {/* Book Table Button */}
